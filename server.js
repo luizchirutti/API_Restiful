6f08d31b2e750df81b08cc3c2fed46f0d40c9bc0
Luiz Chirutti <luizchirutti@gmail.com>
2022-04-25 19:59:13 -0300

    add post client

--- server.js
+++ server.js
@@ -12,12 +12,23 @@ app.get("/clients/:id", function(req, res){
     const { id } = req.params
     const client = data.find(cli => cli.id == id);
 
+    if (!client) return res.status(204).json();
+
     res.json(client);
     });
     
 
-app.post("/clients", function(req, res){});
-app.put("/clients", function(req, res){});
+app.post("/clients", function(req, res){
+
+    const { name, email } = req.body;
+
+    res.json({ name, email });
+
+});
+app.put("/clients/:id", function(req, res){
+
+
+});
 app.delete("/clients", function(req, res){});
 
 
