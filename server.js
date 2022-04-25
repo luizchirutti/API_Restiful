e383420be4cfc5a45e6aa441343a2350a0a15fcb
Luiz Chirutti <luizchirutti@gmail.com>
2022-04-25 15:36:28 -0300

    criando API Restful

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get("/clients")
+app.post("/clients")
+app.put("/clients")
+app.delete("/clients")
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
